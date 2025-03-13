6363e6b435f6bc092a73dd871818714155769f04
Paolav17 <paolavargasmendez.1@gmail.com>
2025-03-13 18:09:53 -0500

    Final Commit

--- script.js
+++ script.js
@@ -176,5 +176,19 @@ document.addEventListener ('DOMContentLoaded',function(){
             modalPython.classList.remove('modalPython--show')
             console.log(modalPython)
         });
-} )
+        const check = document.querySelector(".check")
+        check.addEventListener('click', lenguage);
+
+        function lenguage(){
+            let id=check.checked;
+            if (id==true){
+                location.href="es/index.html";
+            }
+            else{
+                location.href="../index.html";
+            }
+            
+            }
+        }
+)
 
